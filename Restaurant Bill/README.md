9b630ca83334a4282eb47784ea8ded29e49c5092
Nathan Costa <nkyucode@gmail.com>
2023-09-25 14:42:26 -0300

    new test

---
+++ Restaurant Bill/README.md
@@ -0,0 +1,11 @@
+# Restaurant bill
+
+Create a program that reads the value of a dinner. Calculate and inform the value of the waiter's fee (10%) and the total amount to be paid.
+
+Example:
+
+Dinner price: R$80.00
+
+Waiter fee: R$8.00
+
+Total payable: R$88.00
---
+++ Restaurant Bill/restaurantBill.js
@@ -0,0 +1,9 @@
+let dinner = 80;
+let fee = 10;
+let total = 0;
+
+total = dinner + (fee / 100) * dinner;
+
+console.log(`Dinner Value = R$ ${dinner}.00`);
+console.log(`Fee for the waiter = R$ ${(fee / 100) * dinner}.00`);
+console.log(`Total amount payable = R$ ${total}.00`);
