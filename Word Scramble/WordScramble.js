e268058ba7962ab6879a073538d9a376c7329e60
Nathan Costa <nkyucode@gmail.com>
2023-10-25 16:55:05 -0300

    feat: comments to the logic for study case

--- Word Scramble/WordScramble.js
+++ Word Scramble/WordScramble.js
@@ -1,14 +1,21 @@
 function wordScramble(word) {
+    // we create a constant to separate the word
     const lettersInOrder = word.split("");
+    // we create a constant to store the shuffled words in an array
     const scrambledLetters = [];
 
+    // we perform a check inside a loop to reach the entire length of the word
     while (lettersInOrder.length) {
+        // draw a number from 0 to length, we use parseInt to transform into an integer
         const sorted = parseInt(Math.random() * lettersInOrder.length);
+
+        // From the drawn number, we select and remove a letter
         const selectedLetters = lettersInOrder.splice(sorted, 1)[0];
 
+        // we add the letter selected by selectedLetters to our scrambledLetters array
         scrambledLetters.push(selectedLetters);
     }
-
+    // return
     return scrambledLetters.join("");
 }
 
