bca44d34b2ff7abced27ad461f439344750c9564
Nathan Costa <nkyucode@gmail.com>
2023-11-06 17:23:59 -0300

    new challenge completed "teste5"

---
+++ YT[Serliv]/teste05.js
@@ -0,0 +1,7 @@
+// Escreva uma função que receba uma string e retorne o número de caracteres encontrados.
+
+function howManyLetters(value) {
+    return value.length;
+}
+
+console.log(howManyLetters("pneumoultramicroscopico silinovulcanocaniose"));
